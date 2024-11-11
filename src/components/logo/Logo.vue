b227e41483dcc0ad52f45ca6d1dad5c1782a13f6
Arjun P Sunil <arjunpsunil7@gmail.com>
2024-11-12 00:31:38 +0530

    sec sec pc push

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -1,22 +1,23 @@
 <template>
-      <div class="absolute right-[-15px] top-[20px] bg-white rounded-2xl" :class="{'right-[30px]': props.is}">
-        <Button
-          icon="pi pi-bars"
-          severity="help"
-          rounded
-          variant="outlined"
-          aria-label="Favorite"
-          @click="buttonClick"
-        />
+    <div class="top public-sans">
+    <div>
+      <img
+        src="\src\assets\Objects.png"
+        alt=""
+      />
+    </div>
+    <div class="heading flex flex-row">
+      <div v-if="props.was">
+        <h2 class="logo_head">CHAT</h2>
+        <p class="logo_head2">bot</p>
       </div>
+    </div>
+</div>
 </template>
 <script setup>
-import Button from "primevue/button";
 import { inject } from "vue";
 
-
 const props = defineProps(['was'])
-
 
 const buttonClick = inject("clicker");
 </script>
